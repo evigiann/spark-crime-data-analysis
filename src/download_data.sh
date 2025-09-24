8b96eb2a86c3d78c8e7206f8abdfd14534995d90
evangelia <evageliagiannakopoulou@gmail.com>
2025-09-24 11:26:59 +0300

    initial commit

---
+++ src/download_data.sh
@@ -0,0 +1,10 @@
+################this file downloads the data
+
+#main data
+wget https://catalog.data.gov/dataset/crime-data-from-2010-to-2019 #2019
+wget https://catalog.data.gov/dataset/crime-data-from-2020-to-present #today
+
+#secondary data
+wget https://geohub.lacity.org/datasets/lahub::lapd-police-stations/about
+wget http://www.dblab.ece.ntua.gr/files/classes/data.tar.gz
+
